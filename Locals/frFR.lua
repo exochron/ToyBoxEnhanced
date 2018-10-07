f3a7494fd6552ea0f8ae101df3a26e84d6e2cf40
exochron <exochron@gamerstate.de>
2018-10-07 14:43:25 +0200

    2.0.1
    2.0.1
    - code: fixed RestrictedExecution error (#7)
    - Locals: updated Korean by next96
    - Locals: added partial French by JackFuret
    - Locals: added partial Simplified Chinese by yuningning520
    - Locals: added partial Traditional Chinese by jerry99spkk and yuningning520

---
+++ Locals/frFR.lua
@@ -0,0 +1,35 @@
+﻿local ADDON_NAME, ADDON = ...
+
+if (GetLocale() == 'frFR') then
+    local L = ADDON.L or {}
+
+    L["Only usable"] = "Utilisable uniquement"
+    L["Toys"] = "Jouets"
+    L["Usable"] = "Utilisable"
+
+    -- Settings
+
+    -- Source
+    L["Black Market"] = "Marché noir"
+    L["Enchanting"] = "Enchantement"
+    L["Engineering"] = "Ingénierie"
+    L["Jewelcrafting"] = "Joaillerie"
+    L["Leatherworking"] = "Travail du cuir"
+    L["Order Hall"] = "Domaine de classe"
+    L["Pick Pocket"] = "Vol à la tire"
+    L["Treasure"] = "Trésor"
+
+    -- World Event
+    L["Brewfest"] = "Fête des Brasseurs"
+    L["Children's Week"] = "Semaine des enfants"
+    L["Day of the Dead"] = "Jour des morts"
+    L["Feast of Winter Veil"] = "Voile d'hiver"
+    L["Hallow's End"] = "Sanssaint"
+    L["Love is in the Air"] = "De l'amour dans l'air"
+    L["Lunar Festival"] = "Fête lunaire"
+    L["Midsummer Fire Festival"] = "Fête du feu du solstice d'été"
+    L["Pilgrim's Bounty"] = "Bienfaits du pèlerin"
+    L["Pirates' Day"] = "Jour des pirates"
+
+    ADDON.L = L
+end
---
+++ Locals/zhCN.lua
@@ -0,0 +1,31 @@
+﻿local ADDON_NAME, ADDON = ...
+
+if (GetLocale() == 'zhCN') then
+    local L = ADDON.L or {}
+
+    L["Hidden"] = "已隐藏"
+    L["Only usable"] = "只可用"
+    L["Toys"] = "玩具"
+    L["Usable"] = "可用"
+
+    -- Settings
+
+    -- Source
+    L["Black Market"] = "黑市"
+    L["Engineering"] = "工程学"
+    L["Jewelcrafting"] = "珠宝加工"
+    L["Pick Pocket"] = "偷窃"
+    L["Treasure"] = "宝藏"
+
+    -- World Event
+    L["Brewfest"] = "美酒节"
+    L["Children's Week"] = "儿童周"
+    L["Feast of Winter Veil"] = "冬幕节"
+    L["Hallow's End"] = "万圣节"
+    L["Love is in the Air"] = "情人节"
+    L["Lunar Festival"] = "春节"
+    L["Midsummer Fire Festival"] = "仲夏节"
+    L["Pilgrim's Bounty"] = "感恩节"
+
+    ADDON.L = L
+end
