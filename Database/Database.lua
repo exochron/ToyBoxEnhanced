c74e6df51e261b85447c6036906b680943e0e48b
exochron <exochron@gamerstate.de>
2022-10-30 11:13:50 +0100

    - fixed retail delay threshold

--- Database/Database.lua
+++ Database/Database.lua
@@ -1,6 +1,6 @@
 local _, ADDON = ...
 
-ADDON.DELAY_CHECK = 749 -- Threshold of at least loaded toy count (see: bottom of DebugTest) = total - count of hidden flag ( https://wow.tools/dbc/?dbc=toy#page=1&colFilter[3]=0x2 )
+ADDON.DELAY_CHECK = 740 -- Threshold of at least loaded toy count (see: bottom of DebugTest) = total - count of hidden flag ( https://wow.tools/dbc/?dbc=toy#page=1&colFilter[3]=0x2 )
 
 ADDON.db = {}
 
