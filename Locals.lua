796d1c69f03706734cdaad5d8b9c674514618c1e
exochron <exochron@gamerstate.de>
2023-09-06 18:51:17 +0200

    feat(i18n): added itIT and esMX as possible translations

--- Locals.lua
+++ Locals.lua
@@ -110,11 +110,21 @@ elseif locale == "esES" then
     --@localization(locale="esES", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="esES", namespace="Source", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="esES", namespace="Effects", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "esMX" then
+    --@localization(locale="esMX", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="esMX", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="esMX", namespace="Source", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="esMX", namespace="Effects", format="lua_additive_table", handle-unlocalized=comment)@
 elseif locale == "frFR" then
     --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="frFR", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="frFR", namespace="Source", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="frFR", namespace="Effects", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "itIT" then
+    --@localization(locale="itIT", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="itIT", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="itIT", namespace="Source", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="itIT", namespace="Effects", format="lua_additive_table", handle-unlocalized=comment)@
 elseif locale == "koKR" then
     --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="koKR", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
