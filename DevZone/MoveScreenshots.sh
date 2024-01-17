873d689240a97ace9108c59a424e5dda9daedf30
exochron <exochron@freenet.de>
2024-01-17 17:47:43 +0100

    chore: updated screenshots

---
+++ DevZone/MoveScreenshots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd Images || return
+
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 06_options.jpg
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 05_filter-source.jpg
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 04_filter-effects.jpg
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 03_sort.jpg
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 02_favorites.jpg
+mv "`ls -dtr1 ../../../../Screenshots/* | tail -1`" 01_random.jpg
+
+convert -crop 887x752+10+105 01_random.jpg 01_random.jpg
+convert -crop 806x738+10+119 02_favorites.jpg 02_favorites.jpg
+convert -crop 1119x738+10+119 03_sort.jpg 03_sort.jpg
+convert -crop 1221x738+10+119 04_filter-effects.jpg 04_filter-effects.jpg
+convert -crop 1343x738+10+119 05_filter-source.jpg 05_filter-source.jpg
+convert -crop 799x820+1769+130 06_options.jpg 06_options.jpg
