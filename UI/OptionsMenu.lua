d0aa5150b4c65ce452b1405167a64a67f567f326
exochron <exochron@freenet.de>
2024-07-25 19:08:34 +0200

    fix(ui): use modern dropdown menu for toy option menu

--- UI/OptionsMenu.lua
+++ UI/OptionsMenu.lua
@@ -1,6 +1,6 @@
 local ADDON_NAME, ADDON = ...
 
-local function InitMenu(sender, level)
+local function InitDDMenu(sender, level)
     local itemId = sender.itemId
 
     local info
@@ -61,19 +61,56 @@ local function InitMenu(sender, level)
     UIDropDownMenu_AddButton(info, level)
 end
 
+local function CreateContextMenu(owner, root, itemId)
+    if itemId and PlayerHasToy(itemId) then
+        local isFavorite = C_ToyBox.GetIsFavorite(itemId)
+
+        if isFavorite then
+            root:CreateButton(BATTLE_PET_UNFAVORITE, function()
+                C_ToyBox.SetIsFavorite(itemId, false)
+            end)
+        else
+            root:CreateButton(BATTLE_PET_FAVORITE, function()
+                C_ToyBox.SetIsFavorite(itemId, true)
+                SetCVarBitfield("closedInfoFrames", LE_FRAME_TUTORIAL_TOYBOX_FAVORITE, true)
+                HelpTip:Hide(ToyBox, TOYBOX_FAVORITE_HELP)
+            end)
+        end
+
+        local isHidden = itemId and ADDON.settings.hiddenToys[itemId]
+        if isHidden then
+            root:CreateButton(SHOW, function()
+                ADDON.settings.hiddenToys[itemId] = nil
+                ADDON:FilterToys()
+            end)
+        else
+            root:CreateButton(HIDE, function()
+                ADDON.settings.hiddenToys[itemId] = true
+                ADDON:FilterToys()
+            end)
+        end
+
+        root:CreateButton(CANCEL, function() end)
+    end
+end
+
 ADDON.Events:RegisterCallback("OnLoadUI", function()
     local menu
     for i = 1, ADDON.TOYS_PER_PAGE do
         ToyBox.EnhancedLayer["spellButton" .. i]:HookScript("OnClick", function(sender, button)
             if not IsModifiedClick() and not sender.isPassive and button ~= "LeftButton" then
-                if not menu then
-                    menu = CreateFrame("Frame", ADDON_NAME .. "ToyMenu", ToyBox, "UIDropDownMenuTemplate")
-                    UIDropDownMenu_Initialize(menu, InitMenu, "MENU")
-                end
+                if MenuUtil then
+                    MenuUtil.CreateContextMenu(sender, CreateContextMenu, sender.itemID)
+                else
+                    if not menu then
+                        menu = CreateFrame("Frame", ADDON_NAME .. "ToyMenu", ToyBox, "UIDropDownMenuTemplate")
+                        UIDropDownMenu_Initialize(menu, InitDDMenu, "MENU")
+                    end
 
-                menu.itemId = sender.itemID
-                ToggleDropDownMenu(1, nil, menu, sender, 0, 0)
-                PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
+                    menu.itemId = sender.itemID
+                    ToggleDropDownMenu(1, nil, menu, sender, 0, 0)
+                    PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
+                end
             end
         end)
     end
